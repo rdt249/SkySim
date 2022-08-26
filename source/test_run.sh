8095116a410b9658f7e65020d4d2f26ba2e6bab0
Stephen Lawrence <stephen@Stephens-MacBook-Pro.local>
2022-08-25 18:39:24 -0700

    update test_run.sh

--- source/test_run.sh
+++ source/test_run.sh
@@ -1,5 +1,11 @@
 #!
 
-echo "\ttest run with $1"
+echo "copying input.scs ..."
 cp ~/simulation/$1/spectre/schematic/netlist/input.scs dev/input.scs
-echo "\tcopied input.scs"
+
+echo "setting up environment ..."
+source ~/SW_Run/bashrc.IC
+
+echo "running spectre ..."
+$MODELS = home/PDKs/Skywater/V1.10.500/MODELS/SPECTRE/c9fh-3r/Models/
+spectre dev/input.scs -I/$MODELS -raw dev
