5f71598f22a1330529a1b74944614f12527ce936
Stephen Lawrence <stephen@Stephens-MacBook-Pro.local>
2022-08-25 17:10:59 -0700

    renamed main script

--- skysim.py
+++ skysim.py
@@ -1,7 +1,9 @@
 # Stephen Lawrence 2022
 
-import subprocess
-import os
+import subprocess, sys, os
+
+INPUT_PATH = os.getcwd() + '/input.txt'
+OUTPUT_PATH = os.getcwd() + '/output.txt'
 
 # run configuration
 RUN = {
@@ -34,7 +36,13 @@ def CreateRun(run):
     RUN['nodes'] = FindNodes(netlist)
 
 def main():
-    print(0)
+    print(INPUT_PATH)
+    print(OUTPUT_PATH)
 
 if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) > 0:
+        for i in range(0,len(args),2):
+            if args[i] == '-i': INPUT_PATH = args[i + 1]
+            if args[i] == '-o': OUTPUT_PATH = args[i + 1]
     main()
